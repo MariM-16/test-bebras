6a86d108a4b85cd5731220556086af334125b720
Maria Marin <mdmarin1@miuandes.cl>
2025-04-16 02:11:44 -0400

    Improvements to the Django admin, creating custom questions with their other attributes

--- test_bebras/admin.py
+++ test_bebras/admin.py
@@ -1,10 +1,59 @@
 from django.contrib import admin
 from .models import Skill, Question, Choice, ScoringConfiguration, Test, Attempt, Answer
 
-admin.site.register(Skill)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(ScoringConfiguration)
-admin.site.register(Test)
-admin.site.register(Attempt)
-admin.site.register(Answer)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class ScoringConfigurationInline(admin.StackedInline):
+    model = ScoringConfiguration
+    extra = 0
+    max_num = 1
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('short_statement', 'difficulty', 'response_format')
+    list_filter = ('difficulty', 'response_format')
+    search_fields = ('statement',)
+    filter_horizontal = ('skills',)
+
+    def get_inline_instances(self, request, obj=None):
+        inlines = []
+        if obj:
+            inlines.append(ScoringConfigurationInline(self.model, self.admin_site))
+            if obj.response_format == 'choice':
+                inlines.append(ChoiceInline(self.model, self.admin_site))
+        return inlines
+
+    def short_statement(self, obj):
+        return obj.statement[:60]
+    short_statement.short_description = "Question"
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+@admin.register(ScoringConfiguration)
+class ScoringConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('question', 'points_per_difficulty', 'penalty_for_incorrect', 'fixed_penalty', 'allow_no_response')
+    search_fields = ('question__statement',)
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'text', 'is_correct')
+    list_filter = ('is_correct',)
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'maximum_time', 'allow_backtracking')
+    filter_horizontal = ('questions',)
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'answered_at')
+    list_filter = ('answered_at',)
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('user', 'test', 'date_taken', 'score', 'correct_count')
+    list_filter = ('date_taken',)
