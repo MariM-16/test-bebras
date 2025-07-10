25f448889655900a9634bf2e18e2fff347f974fc
Maria Marin <mdmarin1@miuandes.cl>
2025-07-10 02:09:57 -0400

    Cambios visual preguntas choice, cambio de tipo text choice a editor de imagen

--- test_bebras/models.py
+++ test_bebras/models.py
@@ -29,7 +29,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
-    text = RichTextField()
+    text = RichTextUploadingField()
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
