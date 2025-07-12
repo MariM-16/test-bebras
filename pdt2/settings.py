b1e57bf9a9d9bb1a50783da918aaf8fc2b583c30
Maria Marin <mdmarin1@miuandes.cl>
2025-07-11 21:26:03 -0400

    Configure database for Heroku using DATABASE_URL and add psycopg2-binary

--- pdt2/settings.py
+++ pdt2/settings.py
@@ -98,7 +98,10 @@ DATABASES = {
         }
     }
 }
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600, ssl_require=True
+    )
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
